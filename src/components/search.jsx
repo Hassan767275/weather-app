31e3b0bcb34a0d034715c6ab710ad12a34ccf9bc
Hassan767275 <hassanakhtar291@gmail.com>
2026-03-22 20:47:49 -0400

    got the api key to work and now we can display the weather in cities

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 
 export default function Search() {
     const [city, setCity] = useState("")
+    const weatherApiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY
 
     function inputChange(formData) {
         let newCity = formData.get("city")
@@ -9,7 +10,11 @@ export default function Search() {
     }
 
     useEffect(() => {
-        console.log("new city entered")
+        if (city !== "") {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric`)
+                .then(res => res.json())
+                .then(data => console.log(`The weather in the city you searched is ${data.main.temp} celcius`))
+        }
     }, [city])
 
     return (
