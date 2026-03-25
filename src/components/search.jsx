18260fa92f8f5811ee470311ca543d70bce40ca0
Hassan767275 <hassanakhtar291@gmail.com>
2026-03-24 21:53:24 -0400

    adding serach button to form

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,10 +1,11 @@
 export default function Search(props) {
     return (
         <>
-            <h1>Weather App</h1>
+            <h1>How's the sky looking today?</h1>
             <form action={props.inputChange}>
                 <label htmlFor="weather-input">Search a city for weather: </label>
                 <input type="search" id="city-input" name="city"></input>
+                <button type="submit">Search</button>
             </form>        
         </>
     )
