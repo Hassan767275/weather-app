248ea0495064124f38fca10fc83f75b1266d72fb
Hassan767275 <hassanakhtar291@gmail.com>
2026-03-24 21:43:21 -0400

    moved some state speicfic code up one level to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import './App.css'
 import Search from "./components/search"
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [city, setCity] = useState("")
+      const [weatherData, setWeatherData] = useState({})
+      const weatherApiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY
+  
+      function inputChange(formData) {
+          let newCity = formData.get("city")
+          setCity(newCity)
+      }
+  
+      useEffect(() => {
+          if (city !== "") {
+              fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric`)
+                  .then(res => res.json())
+                  .then(data => setWeatherData(data))
+          }
+      }, [city])
+
   return (
     <>
-      <Search />
+      <Search 
+        inputChange={inputChange} 
+        weatherData={weatherData}
+      />
     </>
   )
 }
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from "react"
-
-export default function Search() {
-    const [city, setCity] = useState("")
-    const [weatherData, setWeatherData] = useState({})
-    const weatherApiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY
-
-    function inputChange(formData) {
-        let newCity = formData.get("city")
-        setCity(newCity)
-    }
-
-    useEffect(() => {
-        if (city !== "") {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric`)
-                .then(res => res.json())
-                .then(data => setWeatherData(data))
-        }
-    }, [city])
-
+export default function Search(props) {
     return (
         <>
             <h1>Weather App</h1>
-            <form action={inputChange}>
+            <form action={props.inputChange}>
                 <label htmlFor="weather-input">Search a city for weather: </label>
                 <input type="search" id="city-input" name="city"></input>
-                {Object.keys(weatherData).length > 0 && <p>{weatherData.main.temp}</p>}
             </form>        
         </>
     )
