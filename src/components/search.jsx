de97e416c071b860e8c67e96918385619b835e83
Hassan767275 <hassanakhtar291@gmail.com>
2026-03-24 22:12:06 -0400

    centered div

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,12 +1,14 @@
+import "./search.css"
+
 export default function Search(props) {
     return (
-        <>
+        <div className="search">
             <h1>How's the sky looking today?</h1>
-            <form action={props.inputChange}>
+            <form className="search=form" action={props.inputChange}>
                 <label htmlFor="weather-input">Search a city for weather: </label>
                 <input type="search" id="city-input" name="city"></input>
                 <button type="submit">Search</button>
             </form>        
-        </>
+        </div>
     )
 }
