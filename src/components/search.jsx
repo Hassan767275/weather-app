d71fbbbfe98235f585bd1e5fdb4b299e7b893be2
Hassan767275 <hassanakhtar291@gmail.com>
2026-03-24 22:27:41 -0400

    added some more styling

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,10 +4,9 @@ export default function Search(props) {
     return (
         <div className="search">
             <h1>How's the sky looking today?</h1>
-            <form className="search=form" action={props.inputChange}>
-                <label htmlFor="weather-input">Search a city for weather: </label>
-                <input type="search" id="city-input" name="city"></input>
-                <button type="submit">Search</button>
+            <form className="search-form" action={props.inputChange}>
+                <input type="search" className="city-input" name="city" placeholder="Search a city for weather"></input>
+                <button className="search-button" type="submit">Search</button>
             </form>        
         </div>
     )
