2450dda67307f27eea7d19ce5b6f1d5f78bd5a85
Hassan767275 <hassanakhtar291@gmail.com>
2026-04-06 19:57:16 -0400

    fixed search bar not changing size

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -3,10 +3,22 @@ import "./search.css"
 export default function Search(props) {
     return (
         <div className="flex flex-col items-center">
-            <h1 className="mt-20 text-4xl text-[#0EA5E9] font-bold">How's the sky looking today?</h1>
-            <form className="mt-4" action={props.inputChange}>
-                <input type="search" className="mr-2 bg-[#FFFFFF] border-2 border-[#BAE6FD] rounded-xl p-1" name="city" placeholder="Search a city for weather"></input>
-                <button className="bg-[#0EA5E9] py-1 px-3 text-white rounded-lg" type="submit">Search</button>
+            <h1 
+                className="mt-20 text-4xl text-[#0EA5E9] font-bold">
+                    How's the sky looking today?
+            </h1>
+            <form className="flex items-center w-full max-w-sm md:max-w-md mt-4 mx-auto" action={props.inputChange}>
+                <input 
+                    type="search" 
+                    className="flex-1 mr-2 bg-[#FFFFFF] border-2 border-[#BAE6FD] rounded-xl p-1" 
+                    name="city" 
+                    placeholder="Search a city for weather">
+                </input>
+                <button 
+                    className="bg-[#0EA5E9] py-1 px-3 text-white rounded-lg" 
+                    type="submit">
+                        Search
+                </button>
             </form>        
         </div>
     )
