3a28ea148f34bb9159fc7df5b2e33ebaf55b95e8
Hassan767275 <hassanakhtar291@gmail.com>
2026-04-16 12:24:28 -0400

    rendering multiple element inside 6dayforecast componenets

--- src/App.jsx
+++ src/App.jsx
@@ -40,9 +40,7 @@ function App() {
       {Object.keys(dailyForecast).length > 0 && (
         <>
           <h1 className="text-center mt-2 text-[#3B82F6]">6 Day Forecast</h1>
-          {Object.entries(dailyForecast).map(([day, {maxTemp, minTemp, description}]) => (
-            <SixDayForecast key={day} day={day} maxTemp={maxTemp} minTemp={minTemp} description={description}/>
-          ))}
+          <SixDayForecast dailyForecast={dailyForecast}/>
         </>
       )}
     </>
--- src/components/SixDayForecast.jsx
+++ src/components/SixDayForecast.jsx
@@ -1,15 +1,22 @@
-import {weatherTypes} from "../weatherIcons/weatherIcons.js"
+import { weatherTypes } from "../weatherIcons/weatherIcons.js";
 
-export default function FiveDayForcast({ day, maxTemp, minTemp, description}) {
-    
-    return (
-        <div className="flex flex-col items-center">
-            <h1>{day}</h1>
+export default function FiveDayForcast({ dailyForecast }) {
+  if (!dailyForecast) return null;
+
+  return (
+    <div className="flex">
+      {Object.entries(dailyForecast).map(
+        ([day, { maxTemp, minTemp, description }]) => (
+          <div className="flex flex-col items-center" key={day}>
+            <h1 className="text-2xl text-[#3B82F6] font-bold">{day}</h1>
             <img className="w-16 h-16" src={weatherTypes[description]}></img>
             <div>
-                <h1>{maxTemp}</h1>
-                <h1>{minTemp}</h1>
+              <h1>{maxTemp}</h1>
+              <h1>{minTemp}</h1>
             </div>
-        </div>
-    )
+          </div>
+        ),
+      )}
+    </div>
+  );
 }
